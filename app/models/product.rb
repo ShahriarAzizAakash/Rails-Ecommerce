594693d21c7b7c494e57c2035f64f08533289693
ShahriarAzizAakash <aakashaayat@gmail.com>
2019-03-27 22:48:03 +0600

    cart and line items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
+    before_destroy :not_referenced_by_any_line_item
     mount_uploader :image, ImageUploader
     belongs_to :user, optional: true
+    has_many :line_items
 
     validates :title, :category, :condition, :description, :brand, :price, presence: true
     validates :description, length: {maximum:1000, too_long: "%{count} characters is the maximum allowed!"}
@@ -8,4 +10,13 @@ class Product < ApplicationRecord
     validates :price, numericality: {only_integer: true}, length: {maximum: 7}
 
     CONDITION = %w{New Excellent Mint Fair Poor}
+
+    private 
+
+    def not_referenced_by_any_line_item
+        unless line_items.empty?
+            errors.add(:base, "Line items present")
+            throw :abort
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_202454) do
+ActiveRecord::Schema.define(version: 2019_03_27_160555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "product_id"
+    t.bigint "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title", null: false
@@ -41,4 +56,6 @@ ActiveRecord::Schema.define(version: 2019_03_12_202454) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "products"
 end
