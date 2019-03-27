e3b156b553f1a97d1c06c5e1d427932fae720eea
ShahriarAzizAakash <aakashaayat@gmail.com>
2019-03-28 00:01:46 +0600

    cart completed

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,11 +2,6 @@ class CartsController < ApplicationController
 rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
 
-  # GET /carts
-  # GET /carts.json
-  def index
-    @carts = Cart.all
-  end
 
   # GET /carts/1
   # GET /carts/1.json
@@ -58,7 +53,7 @@ rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
     @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'Cart was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
